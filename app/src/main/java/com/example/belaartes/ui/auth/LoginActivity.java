1a5c713c90f3364581aa6283acfb0ba56dd27aa0
Jonas Carvalho <jonasgabrielsc@gmail.com>
2025-04-21 20:28:45 -0300

    feat: tela login, tela cadastro, tela catálogo produtos e detalhe produtos

--- app/src/main/java/com/example/belaartes/ui/auth/LoginActivity.java
+++ app/src/main/java/com/example/belaartes/ui/auth/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.belaartes.ui.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +14,70 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.belaartes.R;
+import com.example.belaartes.data.model.entities.Usuario;
+import com.example.belaartes.data.repository.UsuarioRepository;
+import com.example.belaartes.ui.cliente.CatalogoProdutosActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        configurarEventos();
+        redirecionaTelaCadastro();
     }
+
+    public void configurarEventos() {
+        btnLogin.setOnClickListener(v -> {
+            String email = editEmail.getText().toString().trim();
+            String senha = editSenha.getText().toString().trim();
+
+            if (email.isEmpty() || senha.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            UsuarioRepository.login(this, email, senha, new UsuarioRepository.LoginCallback() {
+                @Override
+                public void onSuccess(Usuario usuario) {
+                    Toast.makeText(LoginActivity.this, "Login bem-sucedido", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, CatalogoProdutosActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onError(String errorMessage) {
+                    Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+
+    public void redirecionaTelaCadastro() {
+        Button btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
