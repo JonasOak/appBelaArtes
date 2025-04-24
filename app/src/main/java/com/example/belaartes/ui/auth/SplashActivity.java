1015810ee0ad912e3fa41415fce30eecd5d10f50
Layla Nascimento <laylanasci@gmail.com>
2025-04-24 18:41:50 -0300

    ajuste

--- app/src/main/java/com/example/belaartes/ui/auth/SplashActivity.java
+++ app/src/main/java/com/example/belaartes/ui/auth/SplashActivity.java
@@ -22,10 +22,7 @@ public class SplashActivity extends AppCompatActivity{
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-//                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-               Intent intent = new Intent(SplashActivity.this, CarrinhoComprasActivity.class);
-//                Intent intent = new Intent(SplashActivity.this, CatalogoProdutosActivity.class);
-
+              Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
