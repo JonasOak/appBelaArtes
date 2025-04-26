181ac8f79493556f5ac72a96694b80c6edd1b5ed
Eduardo <eduardofreitasfr@gmail.com>
2025-04-26 11:13:46 -0300

    Alterado para tela de produto

--- app/src/main/java/com/example/belaartes/ui/auth/SplashActivity.java
+++ app/src/main/java/com/example/belaartes/ui/auth/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity{
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-              Intent intent = new Intent(SplashActivity.this, RegisterClientActivity.class);
+              Intent intent = new Intent(SplashActivity.this, CatalogoProdutosActivity.class);
                 startActivity(intent);
                 finish();
             }
