640acafc9a39fabb7e553942febab8139baa9ced
eduardo <eduardofreitasfr@gmail.com>
2025-04-28 21:25:21 -0300

    feat: configuração e funcionalidade para remover o produto do carrinho de compras

--- app/src/main/java/com/example/belaartes/adapters/CarrinhoAdapter.java
+++ app/src/main/java/com/example/belaartes/adapters/CarrinhoAdapter.java
@@ -1,13 +1,16 @@
 package com.example.belaartes.adapters;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.belaartes.R;
 import com.example.belaartes.data.model.entities.ItemPedido;
 import com.example.belaartes.data.model.entities.Produto;
+import com.example.belaartes.ui.cliente.CarrinhoComprasActivity;
 
 import java.util.List;
 
@@ -22,8 +26,19 @@ public class CarrinhoAdapter extends RecyclerView.Adapter<CarrinhoAdapter.Carrin
 
     private List<ItemPedido> lista;
 
-    public CarrinhoAdapter(List<ItemPedido> lista) {
+    /**
+     * Configuração para remover item do carrinho
+     */
+    private OnItemRemoveListener onItemRemoveListener;
+
+    public interface OnItemRemoveListener {
+        void onItemRemoved();
+    }
+
+    public CarrinhoAdapter(List<ItemPedido> lista, OnItemRemoveListener listener) {
         this.lista = lista;
+        this.onItemRemoveListener = listener;
+
     }
 
 
@@ -49,6 +64,16 @@ public class CarrinhoAdapter extends RecyclerView.Adapter<CarrinhoAdapter.Carrin
             Bitmap bitmap = BitmapFactory.decodeByteArray(imagemBytes, 0, imagemBytes.length);
             holder.imgProduto.setImageBitmap(bitmap);
         }
+        // Clicar para excluir
+        holder.cleaProduct.setOnClickListener(v -> {
+            lista.remove(holder.getAdapterPosition());
+            notifyItemRemoved(holder.getAdapterPosition());
+            notifyItemRangeChanged(holder.getAdapterPosition(), lista.size());
+
+            if (onItemRemoveListener != null) {
+                onItemRemoveListener.onItemRemoved();
+            }
+        });
     }
 
 
@@ -60,6 +85,7 @@ public class CarrinhoAdapter extends RecyclerView.Adapter<CarrinhoAdapter.Carrin
     public static class CarrinhoViewHolder extends RecyclerView.ViewHolder {
         ImageView imgProduto;
         TextView nomeProduto, descricao, preco, quantidade;
+        ImageButton cleaProduct;
 
         public CarrinhoViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -68,6 +94,11 @@ public class CarrinhoAdapter extends RecyclerView.Adapter<CarrinhoAdapter.Carrin
             descricao = itemView.findViewById(R.id.txtCorProduto);
             preco = itemView.findViewById(R.id.txtPrecoProduto);
             quantidade = itemView.findViewById(R.id.txtQuantidade);
+            cleaProduct = itemView.findViewById(R.id.btn_list_product_Excluir);
         }
+
+
     }
+
+
 }
