cc338d00d99a9b5b4bb5a4997b28623fc246aabc
eduardo <eduardofreitasfr@gmail.com>
2025-05-01 14:51:41 -0300

    feat: Criado class DTO

---
+++ app/src/main/java/com/example/belaartes/data/model/entities/AdminDto.java
@@ -0,0 +1,46 @@
+package com.example.belaartes.data.model.entities;
+
+public class AdminDto {
+    private long countClient;
+    private long countProduct;
+    private long countRequestOrders;
+
+    public AdminDto(long countClient, long countProduct, long countRequestOrders) {
+        this.countClient = countClient;
+        this.countProduct = countProduct;
+        this.countRequestOrders = countRequestOrders;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminDto{" +
+                "countClient=" + countClient +
+                ", countProduct=" + countProduct +
+                ", countRequestOrders=" + countRequestOrders +
+                '}';
+    }
+
+    public long getCountClient() {
+        return countClient;
+    }
+
+    public void setCountClient(long countClient) {
+        this.countClient = countClient;
+    }
+
+    public long getCountProduct() {
+        return countProduct;
+    }
+
+    public void setCountProduct(long countProduct) {
+        this.countProduct = countProduct;
+    }
+
+    public long getCountRequestOrders() {
+        return countRequestOrders;
+    }
+
+    public void setCountRequestOrders(long countRequestOrders) {
+        this.countRequestOrders = countRequestOrders;
+    }
+}
