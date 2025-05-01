faa60fe10b3486a1ac87b707424a87fd2e4b4b98
eduardo <eduardofreitasfr@gmail.com>
2025-05-01 15:09:12 -0300

    feat: organização, prenchemento de campos do produto e funcionalidade para atualizar

--- app/src/main/java/com/example/belaartes/ui/admin/CadastroProdutoActivity.java
+++ app/src/main/java/com/example/belaartes/ui/admin/CadastroProdutoActivity.java
@@ -1,26 +1,43 @@
 package com.example.belaartes.ui.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 
+import com.example.belaartes.data.model.entities.Produto;
+import com.example.belaartes.data.repository.ProdutoRepository;
 import com.example.belaartes.ui.comum.MenuHelper;
 import com.example.belaartes.R;
 
-public class CadastroProdutoActivity extends AppCompatActivity {
+import java.math.BigDecimal;
+import java.util.List;
 
+public class CadastroProdutoActivity extends AppCompatActivity {
+    private int idProduct;
+    private String imgProductBase64;
     private ImageView btnMenu;
+    private Button saveProduct;
+    private EditText productName, productDescription, productPrice, productAmount;
+
+    private Produto productSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_produto);
+        initializeUI();
+        fillInTheField();
+        setupListeners();
+
     }
 
     @Override
@@ -58,6 +75,57 @@ public class CadastroProdutoActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeUI(){
+        this.productName = findViewById(R.id.edtNome);
+        this.productDescription = findViewById(R.id.edtDescricao);
+        this.productPrice = findViewById(R.id.edtPreco);
+        this.productAmount = findViewById(R.id.edtQuantidade);
+        this.saveProduct = findViewById(R.id.btnSalvar);
+        this.productSelected = new Produto();
+        this.idProduct = 0;
+        this.imgProductBase64 = null;
+
+    }
+    private void fillInTheField(){
+        Intent getProductSave = getIntent();
+        this.productSelected= (Produto) getProductSave.getSerializableExtra("produto");
+        if(productSelected != null){
+            this.idProduct = productSelected.getIdProduto();
+            this.productName.setText(productSelected.getNome());
+            this.productDescription.setText(productSelected.getDescricao());
+            this.productPrice.setText(String.valueOf(productSelected.getPreco()));
+            this.productAmount.setText(String.valueOf(productSelected.getEstoque()));
+            this.imgProductBase64 = productSelected.getImagem();
+        }
+
+    }
+
+    private void setupListeners(){
+        saveProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Produto saveProduct = new Produto(productSelected.getIdProduto(), productName.getText().toString(), productDescription.getText().toString(), productSelected.getCategoria(), new BigDecimal(productPrice.getText().toString()), productSelected.getImagem(), Integer.valueOf(productAmount.getText().toString()));
+
+                ProdutoRepository.atualizarProduto(CadastroProdutoActivity.this, saveProduct, new ProdutoRepository.ProdutoCallback() {
+                    @Override
+                    public void onSuccess(List<Produto> produtos) {
+                        runOnUiThread(()->{
+                            Toast.makeText(CadastroProdutoActivity.this, "Produto atualizado com sucesso!", Toast.LENGTH_SHORT).show();
+                        });
+                    }
+
+                    @Override
+                    public void onError(String mensagemErro) {
+                        runOnUiThread(()->{
+                            Toast.makeText(CadastroProdutoActivity.this, "Erro ao atualizar produto!", Toast.LENGTH_SHORT).show();
+                        });
+                    }
+                });
+            }
+        });
+
     }
 
 }
