9e0d6402bb10fc552ccade6e49392019eb0ef841
eduardo <eduardofreitasfr@gmail.com>
2025-05-01 22:18:39 -0300

    feat: class de configuração do usuário

---
+++ app/src/main/java/com/example/belaartes/ui/user/ActivityUserConfig.java
@@ -0,0 +1,39 @@
+package com.example.belaartes.ui.user;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+import com.example.belaartes.R;
+import com.example.belaartes.ui.AppExtends;
+
+public class ActivityUserConfig extends AppExtends {
+    private Button btnDelete, btnDisableAccount, btnLogout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_config);
+        initializeUi();
+        setupListeners();
+    }
+
+    private void initializeUi(){
+        this.btnDelete = findViewById(R.id.profile_delete_user);
+        this.btnDisableAccount = findViewById(R.id.profile_disable_user);
+        this.btnLogout = findViewById(R.id.profile_logout_user);
+
+    }
+
+    private void setupListeners(){
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+}
