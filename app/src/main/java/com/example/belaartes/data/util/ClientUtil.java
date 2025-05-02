6a2d89e1abec7dc0f8389d4c4a2e2a07b10a1762
eduardo <eduardofreitasfr@gmail.com>
2025-05-01 22:15:09 -0300

    feat: Interface para desconectar

---
+++ app/src/main/java/com/example/belaartes/data/util/ClientUtil.java
@@ -0,0 +1,10 @@
+package com.example.belaartes.data.util;
+
+import static com.example.belaartes.data.session.ClientSession.clientSession;
+
+public interface ClientUtil {
+
+     public void logout();
+
+
+}
